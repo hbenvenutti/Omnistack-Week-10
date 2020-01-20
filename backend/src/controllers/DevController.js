b14624afe9ec9a695d2ea02b5c6f1f72b15fb80a
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-01-19 23:24:15 -0300

    Feature Final da semana omnistack; Aplicado WebSocket agora quando um dev e cadastrado e se enquadra no filtro de busca no mobile, ele aparece no mapa sem precisar recarregar ou buscar novamente;

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const {findConnections, sendMessage} = require('../websocket');
 
 // index, show, store, update, destroy
 
@@ -37,6 +38,14 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
+
+            //filtrar as conexões para enviar ao mobile
+            const sendSocketMessageTo = findConnections(
+                {latitude, longitude},
+                techsArray,
+            );
+            
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express =  require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
 const routes = require('./routes');
 
+const {setupWebsocket} = require('./websocket');
+
 const app = express();
+const server = http.Server(app); //Servidor http para usar WebSocket
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://hbenvenutti:L551rp4r1vPIX03CLRd9@cluster0-re0yq.mongodb.net/week10?retryWrites=true&w=majority',
     {
@@ -15,4 +21,4 @@ app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);    //substitui o app.listen(3333)
